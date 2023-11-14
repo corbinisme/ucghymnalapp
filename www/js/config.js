e8e9498eb6f3c176d705f8e5e5b4d9495e13dbb3
Corbin Rose <Rosec@ucgpdc.local>
2023-11-14 09:27:48 -0500

    adding pdf beta

--- www/js/config.js
+++ www/js/config.js
@@ -6,14 +6,7 @@ var config = {
 	vocal_path: "https://ucgweb.s3.amazonaws.com/files/ucg-hymnal-app-v2/hymns-vocal/",
 	//novocal: [100,115,121,124,127,129,133,136,147,171,183,186],
 	novocal: [171],
-	pdf: false,
+	pdf: true,
 	pdfpath: "pdf/"
 	//pdfpath: "https://ucgweb.s3.amazonaws.com/files/ucg-hymnal-app-v2/pdf/"
-}
-
-var configCGI = {
-	brand: "cgi",
-	langs: "en",
-	icon: "icon.png",
-	path: "https://corbinrose.com/sites/hymnal/cgi/mp3/"
 }
