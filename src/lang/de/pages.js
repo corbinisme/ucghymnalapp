2f5f168ae024c3b29fb8a0c366250f15b79ae08b
Corbin Rose <corbinisnotyou@gmail.com>
2025-04-17 08:01:56 -0400

    lang update and adding cordova js

---
+++ src/lang/de/pages.js
@@ -0,0 +1,33 @@
+var pages_de = {
+
+    "about": {
+        title: "About the United Church of God", 
+        content:`
+            <p> <em><strong>Lobe den HERRN! Denn es ist gut, unserem Gott zu singen; denn es ist angenehm, und Lob ist schön. - Psalm 147,1</strong></em></p>
+            <p>Singen war im Laufe der Jahrhunderte ein wesentlicher Bestandteil der Gottesverehrung – von den Engeln, die bei der Erschaffung der Erde sangen (Hiob 38,7), über die Kinder Israels, die Gott für die Befreiung vom ägyptischen Heer lobten (Exodus 15), bis hin zu den Brüdern im Neuen Testament, die angewiesen wurden, Gott in ihren Herzen zu singen und zu lobsingen (Epheser 5,19). Gott möchte, dass wir Freude daran haben, ihn zu preisen.</p>
+            <p>Das Buch der Offenbarung offenbart, dass Gesang auch im kommenden Reich Gottes ein wichtiger Bestandteil der Anbetung sein wird. Offenbarung 14,3 und 15,3 beschreiben, wie die Heiligen ein neues Lied singen, neben dem Lied des Mose und dem Lied des Lammes.</p>
+            <p>Heute wird diese Tradition des Singens fortgeführt, indem wir Gottes Auftrag erfüllen und einen großen Teil unserer Gottesdienste dem gemeinsamen Lobgesang widmen. Inspirierende Texte erinnern uns an die großartigen Wahrheiten seines Wortes. Durch das Singen von Kirchenliedern haben wir die Möglichkeit, unsere tiefste Dankbarkeit gegenüber Gott für seinen Heilsplan für uns und die gesamte Menschheit musikalisch zum Ausdruck zu bringen. Aufgrund der Größe seines wunderbaren Werkes und seiner unglaublichen Liebe verdient Gott unsere tiefste Liebe, Ehre und unseren Respekt durch Lobpreis.</p>
+            <p>Kolosser 3,16 fordert uns auf, Gott mit dankbarem Herzen „Psalmen, Lobgesänge und geistliche Lieder“ zu singen. Wie Sie dem Inhaltsverzeichnis entnehmen können, wurde dieses Gesangbuch unter Berücksichtigung dieser Schriftstelle zusammengestellt.
+            Mögen wir dieses Gesangbuch nutzen, um Gottes Anweisung zu erfüllen: „Ich will den Herrn loben von ganzem Herzen in der Gemeinde der Aufrichtigen und in der Gemeinde“ (Psalm 111,1) und „Ich will dem Herrn singen mein Leben lang, ich will meinem Gott singen, solange ich bin“ (Psalm 104,33).</p>
+            <p>&nbsp;</p>`
+    },
+    "copyright": {
+        title: "Copyright Acknowledgements", 
+        content:`<p>Es wurden umfangreiche Anstrengungen unternommen, die Inhaber des in dieser Gesangbuch-App verwendeten urheberrechtlich geschützten Materials ausfindig zu machen. Nach entsprechender Benachrichtigung wird der Herausgeber in nachfolgenden App-Updates entsprechende Korrekturen vornehmen.</p>`
+    },
+    "ucg": {
+        title: "About the United Church of God", 
+        content: `
+            <h2>Wer wir sind</h2>
+            <p>Diese Gesangbuch-App wird von der United Church of God herausgegeben, einer internationalen Vereinigung mit Pfarrern und Gemeinden in den USA und vielen Ländern weltweit. Unsere Ursprünge gehen auf die Kirche zurück, die Jesus im frühen 1. Jahrhundert gründete. Wir folgen denselben Lehren, Doktrinen und Praktiken, die damals etabliert wurden. Unser Auftrag ist es, das Evangelium vom kommenden Reich Gottes weltweit zu verkünden und alle Völker zu lehren, die Gebote Christi zu befolgen. Wenn Sie mehr über die United Church of God erfahren möchten, besuchen Sie unsere Website unter <a href="https://www.ucg.org">www.ucg.org</a>.</p>
+            <h2>Persönlicher Berater verfügbar</h2>
+            <p>Jesus befahl seinen Nachfolgern, seine Schafe zu weiden. Um diesem Gebot nachzukommen, hat die United Church of God Gemeinden auf der ganzen Welt. In diesen Gemeinden treffen sich Gläubige am Samstag, dem Sabbat, und an biblischen Feiertagen, um aus der Heiligen Schrift zu lernen und Gemeinschaft zu pflegen. Die United Church of God hat sich dem Verständnis und der Praxis des neutestamentlichen Christentums verschrieben. Wir möchten Gottes Lebensweise mit denen teilen, die unseren Erlöser Jesus Christus ernsthaft anbeten und ihm nachfolgen möchten. Unsere Pfarrer stehen für Beratung, Fragen und Bibelerklärungen zur Verfügung.</p>
+            <p>Wenn Sie einen Pfarrer kontaktieren oder eine unserer Gemeinden besuchen möchten, können Sie <a href="https://www.ucg.org/congregations/all">alle UCG-Gemeinden anzeigen</a>, <a href="https://www.ucg.org/find-congregation">nach Postleitzahl suchen</a> oder eine E-Mail an <a href="mailto:info@ucg.org">info@ucg.org</a> senden, um die nächstgelegene Gemeinde zu finden. Wenn Sie anrufen möchten, finden Sie unsere Telefonnummern auf der <a href="https://www.ucg.org/contact/">Kontaktseite</a> auf ucg.org.</p>
+            <p><strong>Postanschrift: </strong>United Church of God, <em>an International
+            Association</em>, PO Box 541027, Cincinnati, OH 45254</p>
+            <p><strong>Weitere Informationen erhalten Sie unter:</strong> Besuchen Sie unsere Website <a href="https://www.ucg.org">www.ucg.org</a>, um unsere Publikationen herunterzuladen oder anzufordern, darunter Ausgaben der <em>Guten Nachricht</em>, Dutzende kostenlose Broschüren mit Bibelstudienhilfen, Beyond Today-Programme und vieles mehr.</p>
+            <p>&nbsp;</p>
+            <hr>`
+    },
+
+};
