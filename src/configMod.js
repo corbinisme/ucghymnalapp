b99f53f058992738ebb52c40d80f3974bccc10cc
Corbin Rose <corbinisnotyou@gmail.com>
2025-06-09 17:01:32 -0400

    removing old file

--- src/configMod.js
+++
@@ -1,9 +0,0 @@
-const config = {
-	brand: "ucg",
-	langs: "en,de,pg,es,fr,nl,it",
-	icon: "icon.png",
-	path: "https://ucgweb.s3.amazonaws.com/files/ucg-hymnal-app-v2/mp3/",
-	vocal_path: "https://ucgweb.s3.amazonaws.com/files/ucg-hymnal-app-v2/hymns-vocal/"
-}
-
-export default config;
